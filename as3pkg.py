9a1c9f320ba3f35228e39d2b68c59d7e2e8aef09
Deron Decamp <decamp.deron@gmail.com>
2024-03-26 14:55:24 -0400

    Update as3pkg.py
    recursively renames an actionscript 3 package, updating all references.

--- as3pkg.py
+++ as3pkg.py
@@ -1,3 +1,4 @@
+
 import os
 import argparse
 
@@ -9,18 +10,18 @@ def replace_package_in_file(filepath, old_package, new_package):
         file.write(content)
 
 def main(directory, old_package, new_package):
-    # Convert package names to path format
+    # convert package names to path format
     old_path = old_package.replace('.', '/')
     new_path = new_package.replace('.', '/')
 
-    # Iterate through all files in the directory
+    # iterate through all files in the directory
     for root, dirs, files in os.walk(directory):
         for file in files:
             if file.endswith(".as"):
                 filepath = os.path.join(root, file)
                 replace_package_in_file(filepath, old_package, new_package)
 
-    # Rename directories to reflect new package name
+    # rename directories to reflect new package name
     old_package_directory = os.path.join(directory, old_path)
     new_package_directory = os.path.join(directory, new_path)
 
@@ -28,9 +29,9 @@ def main(directory, old_package, new_package):
         os.renames(old_package_directory, new_package_directory)
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Rename AS3 package in a directory')
+    parser = argparse.ArgumentParser(description='Rename AS3 package in a directory, recursively')
     parser.add_argument('directory', type=str, help='Path to the directory with AS3 files')
-    parser.add_argument('old_package', type=str, help='Current package name, e.g., com.old.name')
-    parser.add_argument('new_package', type=str, help='New package name, e.g., com.new.name')
+    parser.add_argument('old_package', type=str, help='Current package name, e.g. com.old.name')
+    parser.add_argument('new_package', type=str, help='New package name, e.g. com.new.name')
     args = parser.parse_args()
     main(args.directory, args.old_package, args.new_package)
